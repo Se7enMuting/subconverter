d9435dee9626cf55c982ecda7e4c9cf19536d164
Se7enMuting <se7en@muting.cc>
2025-01-17 23:59:32 +0800

    Generate unique Gist IDs for each path in gistconf.ini
    Added a new feature to upload.cpp to ensure each path generates a unique Gist ID in gistconf.ini. Now, when a new path is processed, a unique Gist ID is created, and this ID is stored under the respective path section in gistconf.ini. This change ensures that different paths have their own IDs, improving the management of Gists.

--- src/handler/upload.cpp
+++ src/handler/upload.cpp
@@ -36,7 +36,6 @@ int uploadGist(std::string name, std::string path, std::string content, bool wri
 
     if(!fileExist("gistconf.ini"))
     {
-        //std::cerr<<"gistconf.ini not found. Skipping...\n";
         writeLog(0, "gistconf.ini not found. Skipping...", LOG_LEVEL_ERROR);
         return -1;
     }
@@ -44,7 +43,6 @@ int uploadGist(std::string name, std::string path, std::string content, bool wri
     ini.parse_file("gistconf.ini");
     if(ini.enter_section("common") != 0)
     {
-        //std::cerr<<"gistconf.ini has incorrect format. Skipping...\n";
         writeLog(0, "gistconf.ini has incorrect format. Skipping...", LOG_LEVEL_ERROR);
         return -1;
     }
@@ -52,29 +50,23 @@ int uploadGist(std::string name, std::string path, std::string content, bool wri
     token = ini.get("token");
     if(!token.size())
     {
-        //std::cerr<<"No token is provided. Skipping...\n";
         writeLog(0, "No token is provided. Skipping...", LOG_LEVEL_ERROR);
         return -1;
     }
 
-    id = ini.get("id");
     username = ini.get("username");
-    if(!path.size())
-    {
-        if(ini.item_exist("path"))
-            path = ini.get(name, "path");
-        else
-            path = name;
+    if (ini.enter_section(path) != 0) {
+        id = "";  // If the section doesn't exist, id is empty
+    } else {
+        id = ini.get("id");
     }
 
     if(!id.size())
     {
-        //std::cerr<<"No gist id is provided. Creating new gist...\n";
-        writeLog(0, "No Gist id is provided. Creating new Gist...", LOG_LEVEL_ERROR);
+        writeLog(0, "No Gist id is provided. Creating new Gist...", LOG_LEVEL_INFO);
         retVal = webPost("https://api.github.com/gists", buildGistData(path, content), getSystemProxy(), {{"Authorization", "token " + token}}, &retData);
         if(retVal != 201)
         {
-            //std::cerr<<"Create new Gist failed! Return data:\n"<<retData<<"\n";
             writeLog(0, "Create new Gist failed!\nReturn code: " + std::to_string(retVal) + "\nReturn data:\n" + retData, LOG_LEVEL_ERROR);
             return -1;
         }
@@ -82,14 +74,12 @@ int uploadGist(std::string name, std::string path, std::string content, bool wri
     else
     {
         url = "https://gist.githubusercontent.com/" + username + "/" + id + "/raw/" + path;
-        //std::cerr<<"Gist id provided. Modifying Gist...\n";
         writeLog(0, "Gist id provided. Modifying Gist...", LOG_LEVEL_INFO);
         if(writeManageURL)
             content = "#!MANAGED-CONFIG " + url + "\n" + content;
         retVal = webPatch("https://api.github.com/gists/" + id, buildGistData(path, content), getSystemProxy(), {{"Authorization", "token " + token}}, &retData);
         if(retVal != 200)
         {
-            //std::cerr<<"Modify gist failed! Return data:\n"<<retData<<"\n";
             writeLog(0, "Modify Gist failed!\nReturn code: " + std::to_string(retVal) + "\nReturn data:\n" + retData, LOG_LEVEL_ERROR);
             return -1;
         }
@@ -99,16 +89,14 @@ int uploadGist(std::string name, std::string path, std::string content, bool wri
     if(json.HasMember("owner"))
         GetMember(json["owner"], "login", username);
     url = "https://gist.githubusercontent.com/" + username + "/" + id + "/raw/" + path;
-    //std::cerr<<"Writing to Gist success!\nGenerator: "<<name<<"\nPath: "<<path<<"\nRaw URL: "<<url<<"\nGist owner: "<<username<<"\n";
     writeLog(0, "Writing to Gist success!\nGenerator: " + name + "\nPath: " + path + "\nRaw URL: " + url + "\nGist owner: " + username, LOG_LEVEL_INFO);
 
-    ini.erase_section();
+    ini.enter_section("common");
     ini.set("token", token);
-    ini.set("id", id);
     ini.set("username", username);
 
     ini.set_current_section(path);
-    ini.erase_section();
+    ini.set("id", id);
     ini.set("type", name);
     ini.set("url", url);
 
